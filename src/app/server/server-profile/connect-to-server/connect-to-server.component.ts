47dbc0cf6a8eaaeff570faa50ef1dcc1ef988f46
Muzucode <seanhickey4516@gmail.com>
2023-07-01 13:38:04 -0400

    updates

--- src/app/server/server-profile/connect-to-server/connect-to-server.component.ts
+++ src/app/server/server-profile/connect-to-server/connect-to-server.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ServersService } from 'src/app/servers.service';
+import { Server } from 'src/types/server';
 
 @Component({
   selector: 'app-connect-to-server',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./connect-to-server.component.scss']
 })
 export class ConnectToServerComponent {
+  constructor(
+    private serversService: ServersService
+  ){}
 
+  currentServer!: Server
+
+  ngOnInit() {
+    this.serversService.currentServer.subscribe(server => {
+      this.currentServer = server
+    })
+  }
+
+  copyCommand()
 }
